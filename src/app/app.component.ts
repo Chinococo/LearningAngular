00f36258a6813a4029f8feeed4d82243b5f8761c
Chinococo <garyliuyahoocom384@gmail.com>
2025-01-15 10:15:49 +0800

    第一個按鈕觸發+標題即時更新

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import { Title } from '@angular/platform-browser';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'LearningAngular';
+export class AppComponent implements OnInit{
+  constructor(private titleService: Title) {}
+  ngOnInit(): void {
+    //元件初始化要加載的物件
+    var title = 'LearningAngularTest';
+    this.titleService.setTitle(title);
+    $("#SubmitButton").click(()=>{
+      alert("Hello World!");
+      console.log("Hello World!");
+    });
+  }
+
 }
