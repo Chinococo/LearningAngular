b66508482b9277f684acf33d52cb836224f359b2
Shaoxuan Liou-劉韶軒-精誠-數位保險應用服務處 <2500039@systex.com.tw>
2025-01-15 12:16:16 +0800

    新增動態載入顯示方法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import {Component, OnInit} from '@angular/core';
 import {bootstrapApplication, Title} from '@angular/platform-browser';
+import { CommonModule } from '@angular/common'; // 确保导入 CommonModule
 import $ from 'jquery';
 import { Modal as BootstrapModal } from 'bootstrap';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule], // 添加 CommonModule
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
   constructor(private titleService: Title) {}
   title:any = "LearningAngular";
+  items = ['Angular', 'React', 'Vue'];
     ngOnInit(): void {
       //元件初始化要加載的物件
       this.titleService.setTitle(this.title);
